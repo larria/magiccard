a0704696c48115a65bda9b9163d8caff9f494f4a
Larkin <la2n@qq.com>
2020-07-31 14:30:00 +0800

    Registetr

--- src/component/PanelHome.js
+++ src/component/PanelHome.js
@@ -6,14 +6,15 @@ import './PanelHome.css'
 function PanelHome(props) {
     return (
         <>
-            <h2>TODO</h2>
-            <p>个人信息</p>
-            <p>炉子</p>
             <div className="user_w">
                 <img src={local.getLocAvatar()} alt="" />
                 <h2 className="user_name">{local.getLocUserName()}</h2>
             </div>
-            
+            <h2>TODO</h2>
+            <p>个人信息</p>
+            <p>炉子</p>
+            <p>背包</p>
+            <p>卡箱</p>
         </>
     );
 }

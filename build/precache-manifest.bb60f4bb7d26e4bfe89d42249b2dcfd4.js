72ed696eaa533c0c52e5d0d5c4eff9f1c021e285
Larkin <la2n@qq.com>
2020-08-06 19:54:51 +0800

    stove

---
+++ build/precache-manifest.bb60f4bb7d26e4bfe89d42249b2dcfd4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "700c09545d944e47754027e5c5059d35",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b23658614ccb8f3397b3",
+    "url": "./static/css/2.fc5a9450.chunk.css"
+  },
+  {
+    "revision": "865a1df7ad3f388b385c",
+    "url": "./static/css/main.2ab5cc91.chunk.css"
+  },
+  {
+    "revision": "b23658614ccb8f3397b3",
+    "url": "./static/js/2.de2acb0c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.de2acb0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "865a1df7ad3f388b385c",
+    "url": "./static/js/main.c6dc4c08.chunk.js"
+  },
+  {
+    "revision": "bab8e20272e0e49fa835",
+    "url": "./static/js/runtime-main.39f4dce7.js"
+  }
+]);

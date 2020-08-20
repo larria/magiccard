c9d32e967d06a05deed2b0c9de7257130fb4f091
Larkin <la2n@qq.com>
2020-08-20 17:42:05 +0800

    slotWithCard

---
+++ build/precache-manifest.4b492c80c3d924f24bf56293106ee3e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acb237856bcab02d8a8c2faaa7e7ceb7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "252660e2ec2142c6b8ce",
+    "url": "./static/css/2.fc5a9450.chunk.css"
+  },
+  {
+    "revision": "942217bc6a4f7a68cdb5",
+    "url": "./static/css/main.c80463b4.chunk.css"
+  },
+  {
+    "revision": "252660e2ec2142c6b8ce",
+    "url": "./static/js/2.2b5f87a0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.2b5f87a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "942217bc6a4f7a68cdb5",
+    "url": "./static/js/main.347fbe50.chunk.js"
+  },
+  {
+    "revision": "bab8e20272e0e49fa835",
+    "url": "./static/js/runtime-main.39f4dce7.js"
+  }
+]);
